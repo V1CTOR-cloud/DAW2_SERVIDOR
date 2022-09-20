d1d1fd757c68e48be948b661d868ac98e2a48339
V1CTOR-CLOUD <victorbg189@gmail.com>
2022-09-20 09:19:29 +0200

    Iniciando composer: con el comando composer install en la raiz del proyecto y retocando cosas en el JSON de composer

---
+++ AP2/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daw2-dwes/mvc-ap2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8baa3caf9179e5ff64d27e5fde9bfe343e1d7724',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daw2-dwes/mvc-ap2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8baa3caf9179e5ff64d27e5fde9bfe343e1d7724',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
